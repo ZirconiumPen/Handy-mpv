9712b067ee45421c402a673f73963883904e0959
ZirconiumPen <zirconiumpen@gmail.com>
2026-01-25 18:33:40 -0500

    General cleanup

--- main.py
+++ main.py
@@ -13,13 +13,22 @@ import config
 
 API_SECRET = config.API_SECRET
 API_ENDPOINT = "https://www.handyfeeling.com/api/handy/v2/"
+CACHE_URL = "https://tugbud.kaffesoft.com/cache"
+# CACHE_URL = "https://handyfeeling.com/api/sync/upload"
 
-TIMEOUT = 10 * 1000  # 10 seconds
+SEC_TO_MS = 10**3
+SEC_TO_NS = 10**9
+RESYNC_COOLDOWN = 3600 * SEC_TO_NS
+# TIMEOUT = 10 * SEC_TO_MS
 
 time_sync_initial_offset = 0
 time_sync_aggregate_offset = 0
 time_sync_average_offset = 0
 time_syncs = 0
+
+
+def get_time_ms():
+    return int(time_ns() / 10**6)
 
 
 HEADERS = {"X-Connection-Key": API_SECRET}
@@ -58,20 +67,20 @@ def get_saved_time():
 
 
 def get_server_time():
-    time_now = int(time_ns() / 1000000)
+    time_now = get_time_ms()
     return int(time_now + time_sync_average_offset + time_sync_initial_offset)
 
 
 def update_server_time():
     global time_sync_initial_offset, time_sync_aggregate_offset, time_sync_average_offset, time_syncs
 
-    send_time = int(time_ns() / 1000000)  # don't ask
+    send_time = get_time_ms()
     r = requests.get(f"{API_ENDPOINT}servertime", headers=HEADERS)
     data = json.loads(r.text)
     server_time = data["serverTime"]
-    print(server_time)
-    time_now = int(time_ns() / 1000000)
-    print(time_now)
+    time_now = get_time_ms()
+    print("Server time:", server_time)
+    print("Time now:", time_now)
     rtd = time_now - send_time
     estimated_server_time_now = int(server_time + rtd / 2)
 
@@ -88,7 +97,7 @@ def update_server_time():
     if time_syncs < 30:
         update_server_time()
     else:
-        print(f"we in sync, Average offset is: {int(time_sync_average_offset)} ms")
+        print(f"we in sync, Average offset is: {time_sync_average_offset:d} ms")
         return
 
 
@@ -110,17 +119,15 @@ def script_2x(script_file):
         edited.append(action)
 
     final = []
-    for x in range(len(edited)):
-        if edited[x]["pos"] == 95:
-            edited[x]["pos"] = 100
-        final.append(edited[x])
+    for idx, action in enumerate(edited):
+        if action["pos"] == 95:
+            action["pos"] = 100
+        final.append(action)
 
-        if x == len(edited) - 1:
+        if idx == len(edited) - 1:
             break
 
-        new_pos = {}
-        new_pos["at"] = int((edited[x + 1]["at"] + edited[x]["at"]) / 2)
-        new_pos["pos"] = 100
+        new_pos = {"at": int((action["at"] + edited[idx + 1]["at"]) / 2), "pos": 100}
         final.append(new_pos)
 
     script["actions"] = final
@@ -128,26 +135,21 @@ def script_2x(script_file):
 
 
 def upload_script(script, double=False):
-
-    if not double:
-        r = requests.post(
-            "https://tugbud.kaffesoft.com/cache",
-            files={"file": open(script, "rb")},
-        )
+    if double:
+        doubled_script = script_2x(script)
+        file_to_use = ("script.funscript", doubled_script[1])
     else:
-        r = requests.post(
-            "https://tugbud.kaffesoft.com/cache",
-            files={"file": ("script.funscript", script[1])},
-        )
+        file_to_use = open(script, "rb")
+    r = requests.post(CACHE_URL, files={"file": file_to_use})
     data = json.loads(r.text)
-    print(data)
+    print("uploading:", data)
     r = requests.put(
         f"{API_ENDPOINT}hssp/setup", json={"url": data["url"]}, headers=HEADERS
     )
     data = json.loads(r.text)
 
 
-print("Getting Handy Status")
+print("Getting Handy Status...")
 r = requests.get(f"{API_ENDPOINT}status", headers=HEADERS)
 data = json.loads(r.text)
 
@@ -159,20 +161,16 @@ if data["mode"] != 1:
     r = requests.put(f"{API_ENDPOINT}/mode", json={"mode": 1}, headers=HEADERS)
     print(r.text)
 
-print("Handy connected, Uploading script!")
+print("Handy connected, uploading script!")
 
 args = parser.parse_args()
-print(args)
 script = find_script(args.file)
-if args.double:
-    upload_script(script_2x(script), True)
-else:
-    upload_script(script)
+upload_script(script, args.double)
 
 
 saved_time = get_saved_time()
 
-if time_ns() - saved_time["last_saved"] < 3600000000000:
+if time_ns() - saved_time["last_saved"] < RESYNC_COOLDOWN:
     time_sync_average_offset = saved_time["time_sync_average_offset"]
     time_sync_initial_offset = saved_time["time_sync_initial_offset"]
 else:
@@ -183,21 +181,19 @@ player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
 player.play(args.file)
 
 
-def sync_play(time=0, play=True):
+def sync_play(time_s=0.0, play=True):
     if not play:
         r = requests.put(f"{API_ENDPOINT}hssp/stop", headers=HEADERS)
         return
+    time_ms = int(time_s * SEC_TO_MS)
 
-    payload = {"estimatedServerTime": get_server_time(), "startTime": time}
+    payload = {"estimatedServerTime": get_server_time(), "startTime": time_ms}
     r = requests.put(f"{API_ENDPOINT}hssp/play", json=payload, headers=HEADERS)
-    print(r.text)
 
 
 def toggle_motion(is_enabled=True):
-    value = player._get_property("playback-time")
-    time_ms = int(value * 1000)
-    print(time_ms)
-    sync_play(time_ms, is_enabled)
+    time_s = player._get_property("playback-time")
+    sync_play(time_s, is_enabled)
 
 
 @player.on_key_press("up")
@@ -218,22 +214,19 @@ def my_q_binding(*args):
 
 @player.event_callback("playback-restart")
 def file_restart(event):
-    value = player._get_property("playback-time")
-    time_ms = int(value * 1000)
-    print(time_ms)
-    sync_play(time_ms)
-    print(f"Now playing at {time_ms}s")
+    time_s = player._get_property("playback-time")
+    sync_play(time_s)
+    print(f"Now playing at {time_s:.02f}s")
 
 
 def video_pause_unpause(name, is_paused):
     if is_paused:
         sync_play(0, False)
         return
-    value = player._get_property("playback-time")
-    if value is None:
+    time_s = player._get_property("playback-time")
+    if time_s is None:
         return
-    time_ms = int(value * 1000)
-    sync_play(time_ms)
+    sync_play(time_s)
 
 
 player.observe_property("pause", video_pause_unpause)
