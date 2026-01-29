c14c8ab78ab30da462ae46c35427f39d54026efc
ZirconiumPen <zirconiumpen@gmail.com>
2026-01-28 21:56:25 -0500

    Update to FW4 and clean up lots of stuff

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 import argparse
 import json
-import sys
-from os.path import exists
+from os.path import isfile
+from pathlib import Path
 from time import time_ns
 
 import mpv
@@ -11,53 +11,49 @@ import requests
 
 import config
 
-API_SECRET = config.API_SECRET
-API_ENDPOINT = "https://www.handyfeeling.com/api/handy/v2"
-CACHE_URL = "https://tugbud.kaffesoft.com/cache"
-# CACHE_URL = "https://handyfeeling.com/api/sync/upload"
+API_ENDPOINT = "https://www.handyfeeling.com/api/handy-rest/v3"
+
+# Two alternatives; if one doesn't work, try the other
+CACHE_URL = "https://handyfeeling.com/api/sync/upload"
+# CACHE_URL = "https://tugbud.kaffesoft.com/cache"
+
+HEADERS = {
+    "X-Connection-Key": config.CONNECTION_KEY,
+    "X-Api-Key": config.APPLICATION_ID,
+}
+HSSP_ID = 1
 
 SEC_TO_MS = 10**3
 SEC_TO_NS = 10**9
 RESYNC_COOLDOWN = 3600 * SEC_TO_NS
-# TIMEOUT = 10 * SEC_TO_MS
 
-time_sync_initial_offset = 0
-time_sync_aggregate_offset = 0
-time_sync_average_offset = 0
-time_syncs = 0
+client_server_offset = 0  # in milliseconds
 
 
 def get_time_ms():
     return int(time_ns() / 10**6)
 
 
-HEADERS = {"X-Connection-Key": API_SECRET}
-
-parser = argparse.ArgumentParser(description="Handy MPV sync Utility")
-parser.add_argument("file", metavar="file", type=str, help="The file to play")
-parser.add_argument("--double", action="store_true", help="enable 2x speed conversion")
-
-# this code is actually really dumb, should refactor, an intern probably
-# did this. I'm just copying the JS code from the site.
+def get_server_time():
+    return int(get_time_ms() + client_server_offset)
 
 
 def save_server_time():
-    if not exists(config.TIME_SYNC_FILE):
+    if not isfile(config.TIME_SYNC_FILE):
         fp = open(config.TIME_SYNC_FILE, "x")
         fp.close()
     with open(config.TIME_SYNC_FILE, "w") as f:
         json.dump(
             {
                 "last_saved": time_ns(),
-                "time_sync_average_offset": time_sync_average_offset,
-                "time_sync_initial_offset": time_sync_initial_offset,
+                "client_server_offset": client_server_offset,
             },
             f,
         )
 
 
 def get_saved_time():
-    if not exists(config.TIME_SYNC_FILE):
+    if not isfile(config.TIME_SYNC_FILE):
         fp = open(config.TIME_SYNC_FILE, "w")
         fp.write('{"last_saved": 0}')
         fp.close()
@@ -66,46 +62,38 @@ def get_saved_time():
         return time
 
 
-def get_server_time():
-    time_now = get_time_ms()
-    return int(time_now + time_sync_average_offset + time_sync_initial_offset)
-
-
-def update_server_time():
-    global time_sync_initial_offset, time_sync_aggregate_offset, time_sync_average_offset, time_syncs
-
+def measure_offset():
     send_time = get_time_ms()
+
     r = requests.get(f"{API_ENDPOINT}/servertime", headers=HEADERS)
-    data = json.loads(r.text)
-    server_time = data["serverTime"]
-    time_now = get_time_ms()
-    print("Server time:", server_time)
-    print("Time now:", time_now)
-    rtd = time_now - send_time
-    estimated_server_time_now = int(server_time + rtd / 2)
+    data = r.json()
+    server_time = data["server_time"]
 
-    # this part here, real dumb.
-    if time_syncs == 0:
-        time_sync_initial_offset = estimated_server_time_now - time_now
-        print(f"initial offset {time_sync_initial_offset} ms")
-    else:
-        offset = estimated_server_time_now - time_now - time_sync_initial_offset
-        time_sync_aggregate_offset += offset
-        time_sync_average_offset = time_sync_aggregate_offset / time_syncs
+    receive_time = get_time_ms()
 
-    time_syncs += 1
-    if time_syncs < 30:
-        update_server_time()
-    else:
-        print(f"we in sync, Average offset is: {time_sync_average_offset:.0f} ms")
-        return
+    rtd = receive_time - send_time
+    estimated_server_time = int(server_time + rtd / 2)
+    offset = estimated_server_time - receive_time
+    return offset
+
+
+def calculate_client_server_offset(n_samples=30):
+    # algorithm from https://www.handyfeeling.com/api/handy-rest/v3/docs/#/UTILS/getServerTime
+    print("Calculating offset...")
+    aggregate_offset = 0
+    for i in range(n_samples):
+        aggregate_offset += measure_offset()
+        print(f"Sample {i}: aggregate offset = {aggregate_offset}ms")
+    average_offset = aggregate_offset / n_samples
+    return average_offset
 
 
 def find_script(video_path):
-    video_name = video_path.replace("." + str.split(video_path, ".")[-1:][0], "")
-    script_path = f"{video_name}.funscript"
-    if exists(script_path):
-        print(f"script found for video: {video_name}")
+    base_name = video_path.with_suffix("")
+    script_path = f"{base_name}.funscript"
+    if not isfile(script_path):
+        print(f"Script not found: {script_path}")
+        return None
     return script_path
 
 
@@ -135,98 +123,118 @@ def script_2x(script_file):
 
 
 def upload_script(script, double=False):
-    if double:
-        doubled_script = script_2x(script)
-        file_to_use = ("script.funscript", doubled_script[1])
-    else:
-        file_to_use = open(script, "rb")
+    file_to_use = script_2x(script) if double else open(script, "rb")
     r = requests.post(CACHE_URL, files={"file": file_to_use})
-    data = json.loads(r.text)
-    print("uploading:", data)
-    r = requests.put(
-        f"{API_ENDPOINT}/hssp/setup", json={"url": data["url"]}, headers=HEADERS
-    )
-    data = json.loads(r.text)
+    data = r.json()
+    url = data["url"]
+    print("Uploading:", url)
+    r = requests.put(f"{API_ENDPOINT}/hssp/setup", json={"url": url}, headers=HEADERS)
 
 
-print("Getting Handy Status...")
-r = requests.get(f"{API_ENDPOINT}/status", headers=HEADERS)
-data = json.loads(r.text)
+def check_connection():
+    r = requests.get(f"{API_ENDPOINT}/mode", headers=HEADERS)
+    if r.status_code != 200:
+        print(f"Bad status: {r.status_code}")
+        return False
+    if not r.text.strip():
+        print("Empty response")
+        return False
 
-if not data["mode"]:
-    print("Couldn't Sync with Handy, Exiting.")
+    try:
+        data = r.json()
+    except ValueError as e:
+        print(f"Invalid JSON: {r.text}")
+        return False
+
+    if data["result"]["mode"] != HSSP_ID:
+        r = requests.put(
+            f"{API_ENDPOINT}/mode2", json={"mode": HSSP_ID}, headers=HEADERS
+        )
+    return True
+
+
+print("Getting Handy status...")
+if not check_connection():
+    print("Couldn't sync with Handy, exiting...")
+    exit()
+print("Handy connected!")
+
+parser = argparse.ArgumentParser(description="Handy MPV sync Utility")
+parser.add_argument("video_path", type=Path, help="The video file to play")
+parser.add_argument("--double", action="store_true", help="Enable 2x speed conversion")
+args = parser.parse_args()
+
+video_name = str(args.video_path)
+
+if not isfile(args.video_path):
+    print(f"File not found: {video_name}")
     exit()
 
-if data["mode"] != 1:
-    r = requests.put(f"{API_ENDPOINT}/mode", json={"mode": 1}, headers=HEADERS)
-    print(r.text)
-
-print("Handy connected, uploading script!")
-
-args = parser.parse_args()
-script = find_script(args.file)
+script = find_script(args.video_path)
+if not script:
+    exit()
+print(f"Script found: {script}")
 upload_script(script, args.double)
-
 
 saved_time = get_saved_time()
 
 if time_ns() - saved_time["last_saved"] < RESYNC_COOLDOWN:
-    time_sync_average_offset = saved_time["time_sync_average_offset"]
-    time_sync_initial_offset = saved_time["time_sync_initial_offset"]
+    client_server_offset = saved_time["client_server_offset"]
 else:
-    update_server_time()
+    client_server_offset = calculate_client_server_offset()
+    print(f"Syncing complete, new offset: {client_server_offset:.02f} ms")
     save_server_time()
 
-player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True)
-player.play(args.file)
+player = mpv.MPV(
+    config=True,
+    input_default_bindings=True,
+    input_vo_keyboard=True,
+    osc=True,
+)
+player.play(video_name)
 
 
-def sync_play(time_s=0.0, play=True):
-    if not play:
-        r = requests.put(f"{API_ENDPOINT}/hssp/stop", headers=HEADERS)
-        return
+def stop_handy():
+    requests.put(f"{API_ENDPOINT}/hssp/stop", headers=HEADERS)
+
+
+def play_handy(time_s=0.0):
     time_ms = int(time_s * SEC_TO_MS)
-
-    payload = {"estimatedServerTime": get_server_time(), "startTime": time_ms}
-    r = requests.put(f"{API_ENDPOINT}/hssp/play", json=payload, headers=HEADERS)
-
-
-def toggle_motion(is_enabled=True):
-    time_s = player._get_property("playback-time")
-    sync_play(time_s, is_enabled)
+    payload = {"server_time": get_server_time(), "startTime": time_ms}
+    requests.put(f"{API_ENDPOINT}/hssp/play", json=payload, headers=HEADERS)
 
 
 @player.on_key_press("up")
 def my_up_binding(*args):
-    toggle_motion(False)
+    stop_handy()
 
 
 @player.on_key_press("down")
-def my_up_binding(*args):
-    toggle_motion(True)
+def my_down_binding(*args):
+    time_s = player._get_property("playback-time")
+    play_handy(time_s)
 
 
 @player.on_key_press("q")
 def my_q_binding(*args):
-    global player
     player.command("quit")
 
 
 @player.event_callback("playback-restart")
 def file_restart(event):
     time_s = player._get_property("playback-time")
-    sync_play(time_s)
+    play_handy(time_s)
     print(f"Now playing at {time_s:.02f}s")
 
 
 def video_pause_unpause(name, is_paused):
     if is_paused:
-        sync_play(0, False)
+        stop_handy()
         return
     time_s = player._get_property("playback-time")
     if time_s is None:
         return
-    sync_play(time_s)
+    play_handy(time_s)
 
 
 player.observe_property("pause", video_pause_unpause)
@@ -235,4 +243,4 @@ player.observe_property("pause", video_pause_unpause)
 try:
     player.wait_for_playback()
 finally:
-    sync_play(0, False)
+    stop_handy()
